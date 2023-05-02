f73c00d7dc96ba22a395b7d3ce086430e51e38d5
Kristian Berg <kris.as.berg@gmail.com>
2023-05-01 23:27:24 -0600

    problem 1

--- dags/process_equities.py
+++ dags/process_equities.py
@@ -7,16 +7,12 @@ from tqdm import tqdm
 
 from sqlalchemy import create_engine
 
-
 from airflow.decorators import dag, task
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 DATA_PATH = f"{os.getcwd()}/data/"
 os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
-
-hook = PostgresHook(postgres_conn_id='pg_conn')
-engine = create_engine(hook.get_uri())
 
 # !!! Problem 1 !!! 
 @dag(
@@ -30,6 +26,7 @@ def process_equities():
     """
         Creates our postgres table to store our data on equities
     """
+    
     create_equities_table = PostgresOperator(
         task_id="create_equities_table",
         postgres_conn_id="pg_conn",
@@ -47,44 +44,34 @@ def process_equities():
                 "Volume" INTEGER
             );"""
     )
-
-
     @task
     def get_stock_market_data() -> None:
         """
         downloads dataset from kaggle
         """
-        kaggle.api.authenticate()
-        kaggle.api.dataset_download_files(
-            'jacksoncrow/stock-market-dataset', 
-            path=DATA_PATH, 
-            unzip=True
-        )
+        # check if the metadate file exists
+        if not os.path.exists(os.path.join(DATA_PATH, "symbols_valid_meta.csv")):
+            kaggle.api.authenticate()
+            kaggle.api.dataset_download_files(
+                'jacksoncrow/stock-market-dataset',
+                path=DATA_PATH,
+                unzip=True  
+            )
+
+            metadata_df = pd.read_csv(f'{DATA_PATH}/symbols_valid_meta.csv')
+            hook = PostgresHook(postgres_conn_id='pg_conn')
+            engine = hook.get_sqlalchemy_engine()
+            for dirname, _, filenames in os.walk(DATA_PATH):
+                for filename in tqdm(filenames[1:]): # skip meta data file
+                    ticker = filename[:-4] # cut off '.csv' to get the ticker
+                    name = metadata_df[metadata_df['NASDAQ Symbol'] == ticker]['Security Name'].iloc[0] 
+                    ticker_path = os.path.join(dirname, filename)
+                    ticker_df = pd.read_csv(ticker_path)
+                    ticker_df['Symbol'] = ticker
+                    ticker_df['Security Name'] = name 
+                    ticker_df.to_sql('equities', engine, if_exists='append', index=False)
 
 
-    @task
-    def load_stock_market_data():
-        """
-            builds a pandas datframe from all the csvs in the created data folder
-            loads the dataframe into the SQL table
-        """
-
-        metadata_df = pd.read_csv(f'{DATA_PATH}/symbols_valid_meta.csv')
-        tickers = []
-
-        for dirname, _, filenames in os.walk(DATA_PATH):
-            for filename in tqdm(filenames[1:]): # skip meta data file
-                ticker = filename[:-4] # cut off '.csv' to get the ticker
-                name = metadata_df[metadata_df['NASDAQ Symbol'] == ticker]['Security Name'].iloc[0] 
-                ticker_path = os.path.join(dirname, filename)
-                ticker_df = pd.read_csv(ticker_path)
-                ticker_df['Symbol'] = ticker
-                ticker_df['Security Name'] = name 
-                tickers.append(ticker_df)
-
-        equities_df = pd.concat(tickers)
-        equities_df.to_sql('equities', engine, if_exists='replace', chunksize=1000)
-
-    [create_equities_table] >> get_stock_market_data() >> load_stock_market_data()
+    [create_equities_table] >> get_stock_market_data()
 
 dag = process_equities()
